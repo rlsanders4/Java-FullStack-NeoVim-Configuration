7ff1b447c4e05443e8049d1061f0bc32674ae7ba
Ralph-Sanders <ralph.sanders@ibm.com>
2025-07-19 21:44:12 -0500

    update typescript server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return {
         config = function()
             -- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "jdtls" },
+                ensure_installed = { "lua_ls", "ts_ls", "jdtls" },
             })
         end
     },
@@ -47,7 +47,7 @@ return {
             })
 
             -- setup the typescript language server
-            lspconfig.tsserver.setup({
+            lspconfig.ts_ls.setup({
                 capabilities = capabilities,
             })
 
